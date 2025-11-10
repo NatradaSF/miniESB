2c502c17cfb9f5e01a23ba8d29610f83cfd4a245
NatradaSF <natrada.ka@sfis.co.th>
2025-11-10 20:50:44 +0700

    [UPDATED] Move position of LocalDateTime.

--- src/main/java/sf/sfis/miniesb/service/SubscribeScheduledService.java
+++ src/main/java/sf/sfis/miniesb/service/SubscribeScheduledService.java
@@ -12,16 +12,16 @@ public class SubscribeScheduledService {
 	@Autowired
 	private SubscribeRequestService subscribeRequestService;
 
-	LocalDateTime today = LocalDateTime.now().withHour(0).withMinute(0).withSecond(0).withNano(0);
-	LocalDateTime tomorrowFrom = LocalDateTime.now().plusDays(1).withHour(0).withMinute(0).withSecond(0).withNano(0);
-	LocalDateTime tomorrowTo = LocalDateTime.now().plusDays(1).withHour(23).withMinute(59).withSecond(59).withNano(999999999);
+	LocalDateTime today;
+	LocalDateTime tomorrowFrom;
+	LocalDateTime tomorrowTo;
 
 	@Scheduled(cron = "1 0 0 * * ?") //ทุกๆ เที่ยงคืนเลยไป 1 วิของวันถัดไป เพื่อรับข้อมูลของ Common Counter
 	public void subscribeAndRequestCcatab() {
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
-//		tomorrow = LocalDateTime.now().plusDays(1).withHour(0).withMinute(0).withSecond(0).withNano(0);
+		tomorrowFrom = LocalDateTime.now().plusDays(1).withHour(0).withMinute(0).withSecond(0).withNano(0);
+		tomorrowTo = LocalDateTime.now().plusDays(1).withHour(23).withMinute(59).withSecond(59).withNano(999999999);
 		String startTime = tomorrowFrom.format(formatter);
-//		tomorrowTo = LocalDateTime.now().plusDays(1).withHour(23).withMinute(59).withSecond(59).withNano(999999999);
 		String endTime = tomorrowTo.format(formatter);
 		String dataType = "pl_desk";
 
@@ -33,9 +33,9 @@ public class SubscribeScheduledService {
 	@Scheduled(cron = "0 7 17 * * ?") // ทุกๆ 17.07 รับข้อมูล Update ระหว่างวัน และรองรับดึงข้อมูลของวันถัดไปด้วย
 	public void subscribeAfttab() {
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
-//		today = LocalDateTime.now().withHour(0).withMinute(0).withSecond(0).withNano(0);
+		today = LocalDateTime.now().withHour(0).withMinute(0).withSecond(0).withNano(0);
 		String startTime = today.format(formatter);
-//		tomorrow = LocalDateTime.now().plusDays(1).withHour(23).withMinute(59).withSecond(59).withNano(999999999);
+		tomorrowTo = LocalDateTime.now().plusDays(1).withHour(23).withMinute(59).withSecond(59).withNano(999999999);
 		String endTime = tomorrowTo.format(formatter);
 		String dataType = "pl_turn";
 
@@ -45,9 +45,9 @@ public class SubscribeScheduledService {
 	@Scheduled(cron = "0 0 17 * * ?") // ทุกๆ 5 โมงเย็น รับข้อมูลของ Flight วันถัดไป โดยใช้ Subscribe เดิมที่รันไว้ตอน 00.01
 	public void requestAfttab() {
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
-//		LocalDateTime tomorrow = LocalDateTime.now().plusDays(1).withHour(0).withMinute(0).withSecond(0).withNano(0);
+		tomorrowFrom = LocalDateTime.now().plusDays(1).withHour(0).withMinute(0).withSecond(0).withNano(0);
 		String startTime = tomorrowFrom.format(formatter);
-//		tomorrow = LocalDateTime.now().plusDays(1).withHour(23).withMinute(59).withSecond(59).withNano(999999999);
+		tomorrowTo = LocalDateTime.now().plusDays(1).withHour(23).withMinute(59).withSecond(59).withNano(999999999);
 		String endTime = tomorrowTo.format(formatter);
 		String dataType = "pl_turn";
 
